c53a72ce86a150ba1497bfbb55496981dde5bbdb
Bashir Ahmed <bashir0609@users.noreply.github.com>
2025-07-04 21:08:08 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "CSV Toolkit",
-  description: "A simple tool to convert JSON to CSV.",
+  description: "A suite of tools for CSV and JSON manipulation.",
 };
 
 export default function RootLayout({
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="relative">{children}</div>
+      </body>
     </html>
   );
 }
