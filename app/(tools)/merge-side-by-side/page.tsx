be572b1e5a0dfa9bbcd781e37d6ac0cf6b4beff9
Bashir Ahmed <bashir0609@users.noreply.github.com>
2025-07-04 22:11:23 +0600

    Create page.tsx

---
+++ app/(tools)/merge-side-by-side/page.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from '../../components/DashboardLayout';
+import ComingSoon from '../../components/ComingSoon';
+
+export default function MergeSideBySidePage() {
+  return (
+    <DashboardLayout>
+      <ComingSoon title="Merge Side-by-Side" />
+    </DashboardLayout>
+  );
+}
