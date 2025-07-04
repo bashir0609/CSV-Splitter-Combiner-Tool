728854f49c81712570bcf5845b6184d179ecbd22
Bashir Ahmed <bashir0609@users.noreply.github.com>
2025-07-04 22:11:00 +0600

    Create page.tsx

---
+++ app/(tools)/join-on-column/page.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from '../../components/DashboardLayout';
+import ComingSoon from '../../components/ComingSoon';
+
+export default function JoinOnColumnPage() {
+  return (
+    <DashboardLayout>
+      <ComingSoon title="Join on Column (VLOOKUP)" />
+    </DashboardLayout>
+  );
+}
