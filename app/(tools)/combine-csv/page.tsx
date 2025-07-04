ac0ec130dd492c750240aba59252e66c571386e3
Bashir Ahmed <bashir0609@users.noreply.github.com>
2025-07-04 22:10:40 +0600

    Create page.tsx

---
+++ app/(tools)/combine-csv/page.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from '../../components/DashboardLayout';
+import ComingSoon from '../../components/ComingSoon';
+
+export default function CombineCsvPage() {
+  return (
+    <DashboardLayout>
+      <ComingSoon title="Combine CSVs (Append Rows)" />
+    </DashboardLayout>
+  );
+}
