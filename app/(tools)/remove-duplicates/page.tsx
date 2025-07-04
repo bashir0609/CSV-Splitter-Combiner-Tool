5eda00e05968bb6eca334eafb4eadfa0996508b0
Bashir Ahmed <bashir0609@users.noreply.github.com>
2025-07-04 22:11:45 +0600

    Create page.tsx

---
+++ app/(tools)/remove-duplicates/page.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from '../../components/DashboardLayout';
+import ComingSoon from '../../components/ComingSoon';
+
+export default function RemoveDuplicatesPage() {
+  return (
+    <DashboardLayout>
+      <ComingSoon title="Remove Duplicates" />
+    </DashboardLayout>
+  );
+}
